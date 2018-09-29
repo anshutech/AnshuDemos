c4bd9ca252085686a92168cb5990b4184ee98c9b
anshutech <thakur.suchi@gmail.com>
2018-09-29 20:08:12 +0530

    Warning CPU
    Adding the last demo code

---
+++ Cryptography/warningcpu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+namespace warningcpu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            int counter;//it counts that how many times the loop runs
+            counter=1;//value of the counter
+            while(counter<=100000)
+            {
+                Console.WriteLine(Convert.ToString(counter));
+                counter++;
+            }
+            Thread.Sleep(50000);
+          goto start;
+        }
+    }
+}
